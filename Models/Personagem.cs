46a3fbf53f9678743ac8a150fa8e4379b0dd6cf9
edgar <edgar@DESKTOP-OV36AK6>
2023-11-05 17:36:24 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Personagem.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace GameOfThronesAPI.Models;
+
+internal class Personagem
+{
+
+    [JsonPropertyName("name")]
+    public string? Nome { get; set; }
+    [JsonPropertyName("gender")]
+    public string? Genero { get; set; }
+    [JsonPropertyName("born")]
+    public string? DataNascimento { get; set; }
+    [JsonPropertyName("died")]
+    public string? DataMorte { get; set; }
+    [JsonPropertyName("titles")]
+    public List<string>? Titulos { get; set; }
+
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Gênero: {Genero}");
+        Console.WriteLine($"Data de nascimento: {DataNascimento}");
+        Console.WriteLine($"Data de morte: {DataMorte}");
+        Console.WriteLine("Titulos: ");
+        foreach ( string item in Titulos)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using GameOfThronesAPI.Models;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    Console.WriteLine("Escolha o id de personagem que deseja verificar os dados: ");
+    int escolha = Console.Read();
+   
+        if (escolha != 0)
+        {
+        try
+        {
+            string json = await client.GetStringAsync($"https://www.anapioficeandfire.com/api/characters/{escolha}");
+            Personagem personagem = JsonSerializer.Deserialize<Personagem>(json);
+            personagem.ExibirDetalhes();
+        }catch (Exception ex)
+        {
+            Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+        }
+        } else
+    {
+        Console.WriteLine($"Escolha um número válido");
+    }
+  
+
+}
